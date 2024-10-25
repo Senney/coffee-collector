1c407d9d0282de835362b880b1b60710297e0396
Sean Heintz <sean.heintz@gmail.com>
2024-10-24 20:14:03 -0600

    Add user registration

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,6 +1,19 @@
 class RegisterController < ApplicationController
   allow_unauthenticated_access
 
-  def show
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to root_path, notice: "Registration successful."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email_address, :password)
   end
 end
